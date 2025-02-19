b149388f09d82a1d40b667cd750c7aafd9ac2eba
Aniebiet Francis <102663124+Anie22@users.noreply.github.com>
2025-02-19 17:17:56 +0100

    updates

--- menu/urls.py
+++ menu/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("qrcodeBar.urls")),
-    path('/menu/download', include("menuFile.urls")),
+    path('/menu/', include("menuFile.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
